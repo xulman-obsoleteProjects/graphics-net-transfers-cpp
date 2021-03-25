01d522d315abfd731b3102a2f6dad8d9cb0a34f5
Vladimír Ulman <ulman@fi.muni.cz>
2021-03-25 09:37:25 +0100

    CHG: Sends two "display request batches" to the same server, functional solution

--- main.cpp
+++ main.cpp
@@ -7,22 +7,45 @@
 int main(int argc,char** argv)
 {
 	try {
+		std::string msg("v1 points 2 dim 3 10 7.7 8.8 9.9 5 2 11 17.7 8.8 9.9 5 3");
+		std::string msG("v1 points 2 dim 3 110 7.7 8.8 19.9 5 2 111 17.7 8.8 19.9 5 3");
+
 		using namespace Poco::Net;
-		HTTPClientSession session("www.seznam.cz",80);
-		std::cout << "is connected? " << session.connected() << "\n"; //not yet
+		HTTPClientSession session("localhost",8765);
+		HTTPRequest request(HTTPRequest::HTTP_POST, "/simViewer");
 
-		HTTPRequest request(HTTPRequest::HTTP_GET, "/");
-		session.sendRequest(request);
-		//std::ostream &postDataGoesHere = session.sendRequest(request);
-		std::cout << "is connected? " << session.connected() << "\n"; //already is
+		{
+		request.setContentLength(msg.size());
+		std::ostream &requestBody = session.sendRequest(request);
+		requestBody << msg;
+		}
 
+		{
+		request.setContentLength(msG.size());
+		std::ostream &requestBody = session.sendRequest(request);
+		requestBody << msG;
+		}
+
+		/*
+		//hear back some acknowledgement:
 		HTTPResponse response;
-		session.receiveResponse( response );
+		std::istream &responseBody = session.receiveResponse(response);
 
+		if (response.getStatus() != HTTPResponse::HTTP_OK)
+			std::cerr << "couldn't transfer the message!\n";
+		else
+			std::cout << "message transferred!\n";
+
+		/*
+		//verbose report of what the receiving party has to say:
 		std::cout << "got back: status = " << response.getStatus() << "\n";
 		std::cout << "content starts:\n";
 		response.write( std::cout );
+		std::cout << "content middle:\n";
+		std::string resMsg;
+		while (responseBody >> resMsg) std::cout << resMsg << "\n";
 		std::cout << "content ends:\n";
+		*/
 	}
 	catch (Poco::Net::NetException& e) {
 		std::cout << "Got exception: " << e.what() << "\n";
